3489d778d613e0ce5c147cb8b1892d2cf891c3d2
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 19:19:53 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -30,10 +30,10 @@ export async function getStaticProps() {
 }
 const About = ({ posts,blockMap }) => {
     return (
-<div title={BLOG.about} description={BLOG.description} className='about'>
+<div title={BLOG.about} description={BLOG.description} className='about' >
   <div className="flex justify-center ">
      <Tilt 
-              className="my3d shadow-2xl shadow-gray-500 rounded-xl  bg-slate-200 dark:bg-slate-600 max-w-3xl 
+              className="my3d shadow-2xl shadow-gray-500 rounded-xl  bg-slate-200 dark:bg-slate-600 max-w-[80VW] 
               bg-[url('../public/images/mac.jpg')]"
               perspective={1500}
               glareEnable={true}
@@ -44,7 +44,7 @@ const About = ({ posts,blockMap }) => {
               scale={1.02}
             >
                
-              <div className="my3din flex flex-col justify-center items-center flex-wrap p-8">
+              <div className="my3din flex flex-col justify-center items-center flex-wrap p-12">
                 {/*<Image src={LOGOIMG} alt={BLOG.title}  height={180}  />*/}
                 <NotionRenderer blockMap={blockMap} frontMatter={{}} subPageTitle={null} />
               </div>
