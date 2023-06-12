3b1067fa15e40341af60d77c5a9c3ab48c41abde
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 12:05:33 +0800

    Update InfoCard.js

--- components/Card/InfoCard.js
+++ components/Card/InfoCard.js
@@ -1,21 +1,7 @@
-import { useState } from 'react'
-import BlogPost from '@/components/BlogPost'
-import Container from '@/components/Container'
-import Tags from '@/components/Common/Tags'
-import PropTypes from 'prop-types'
-import { lang } from '@/lib/lang'
-import { useRouter } from 'next/router'
-
-const InfoCard = ({ tags, currentTag }) => {
-  const { locale } = useRouter()
-  const t = lang[locale]
-
-
+import CardTags from "./CardTag"
+const InfoCard = ({tags, currentTag  }) => {
   return (
-    
-      <Tags tags={tags} currentTag={currentTag} />
-      
+<CardTags tags={tags} currentTag={currentTag} />  
   )
 }
-
 export default InfoCard
