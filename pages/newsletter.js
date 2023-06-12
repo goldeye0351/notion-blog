1a89cd4623f33d1d14007ef9caf95fad4db51be0
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 13:26:14 +0800

    Update newsletter.js

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,12 +1,13 @@
 import Container from '@/components/Container'
 import BlogPost from '@/components/BlogPost'
 import NewsletterHero from '@/components/Hero/Newsletter'
-import { getAllPosts, getPostBlocks } from '@/lib/notion'
+import { getAllPosts, getAllTagsFromPosts, getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
+import InfoCard from '@/components/Card/InfoCard'
 
 export async function getStaticProps() {
   const posts = await getAllPosts({ onlyNewsletter: true })
-
+  const tags = getAllTagsFromPosts(posts)
   const heros = await getAllPosts({ onlyHidden: true })
   const hero = heros.find((t) => t.slug === 'newsletter')
 
@@ -21,16 +22,17 @@ export async function getStaticProps() {
   return {
     props: {
       posts,
+      tags,
       blockMap
     },
     revalidate: 1
   }
 }
 
-const newsletter = ({ posts, blockMap }) => {
+const newsletter = ({ tags, posts, blockMap }) => {
   return (
     <Container title={BLOG.newsletter} description={BLOG.description}>
-      <NewsletterHero blockMap={blockMap} />
+      <NewsletterHero  tags={tags} posts={posts} blockMap={blockMap} />
       {posts.map((post) => (
         <BlogPost key={post.id} post={post} />
       ))}
