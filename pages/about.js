b0219989d3c25da422e42350384e8af0cf10b3d3
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 18:01:00 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,13 @@
 import Image from "next/image"
-import LOGOIMG from "../public/images/logo.jpg"
-import LVIMG from "../public/images/travel.jpg"
-import Container from '@/components/Container'
-import BlogPost from '@/components/BlogPost'
-import AboutHero from '@/components/Hero/About'
+import LOGOIMG from "../public/images/mac.jpg"
 import { getAllPosts, getAllTagsFromPosts,getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
+import NotionRenderer from '@/components/Post/NotionRenderer'
+import Tilt from 'react-parallax-tilt'
 
 export async function getStaticProps() {
   const posts = await getAllPosts({ onlyNewsletter: false })
   const tags = getAllTagsFromPosts(posts)
-
   const heros = await getAllPosts({ onlyHidden: true })
   const hero = heros.find((t) => t.slug === 'about')
 
@@ -31,12 +28,29 @@ export async function getStaticProps() {
     revalidate: 1
   }
 }
-const About = ({ posts, tags,  blockMap }) => {
+const About = ({ posts,blockMap }) => {
     return (
-
-        <Container title={BLOG.about} description={BLOG.description}>
-        <AboutHero blockMap={blockMap} tags={tags} posts={posts} />
-        </Container>
+<div title={BLOG.about} description={BLOG.description} className='about'>
+  <div className="flex justify-center ">
+     <Tilt 
+              className="my3d shadow-2xl shadow-gray-500 rounded-xl  bg-slate-200 dark:bg-slate-600 max-w-3xl 
+              bg-[url('../public/images/mac.jpg')]"
+              perspective={1500}
+              glareEnable={true}
+              glarePosition={'all'}
+              glareMaxOpacity={0.5}
+              glareColor="#000000"
+              glareBorderRadius="12px"
+              scale={1.02}
+            >
+               
+              <div className="my3din flex flex-col justify-center items-center flex-wrap p-8">
+                {/*<Image src={LOGOIMG} alt={BLOG.title}  height={180}  />*/}
+                <NotionRenderer blockMap={blockMap} frontMatter={{}} subPageTitle={null} />
+              </div>
+    </Tilt>
+ </div>
+</div>
 
 )}
 export default About
