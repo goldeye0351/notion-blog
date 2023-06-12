814ca36db8310f942583b2585fa27c3e4a46d38a
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 12:05:52 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -11,28 +11,27 @@ const BlogPost = ({ index , post }) => {
       <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
       <article
           key={post.id}
-          className='group hover:ring-2 shadow-lg dark:shadow-emerald-900  hover:scale-105 duration-500 overflow-hidden relative mb-5 md:mb-8 cursor-pointer rounded-xl p-5'
+          className='group hover:ring-2 shadow-lg dark:shadow-emerald-900  hover:scale-105 duration-500 overflow-hidden relative my-5 md:my-8 cursor-pointer rounded-xl p-5'
         >
          <div
                 id='blog-ID'
                 key={post.id}
-                className={`md:h-96 h-36 w-full md:flex justify-between  ${index % 2 === 1 ? 'flex-row-reverse ' : ''}
+                className={`h-96 w-full md:flex justify-between  ${index % 2 === 1 ? 'flex-row-reverse ' : ''}
                 overflow-hidden rounded-xl `}>
 
                 {/* 文字内容 */}
-            <div className='relative my-auto mx-auto w-5/12 flex justify-center flex-col items-center  self-center  '>
+            <div className='relative my-auto mx-auto w-5/12 flex justify-center flex-col items-center  self-center  z-10'>
                 <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100'>{post.title}</h2>
-                <span className='text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
+                <span className='hidden md:block text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
                   <FormattedDate date={post.date} />
                 </span>
-                <p className='font-light hidden md:block leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
-
+                <p className='font-light leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
             </div>
                 {/* 图片封面 */}
-
-                <div className='md:w-7/12 w-full overflow-hidden h-96 bg-center bg-cover rounded-lg ' style={{ backgroundImage: `url('${post?.page_cover}')` }} />
-
-
+            <div className="md:relative md:w-7/12 z-0 
+                            w-full h-full overflow-hidden bg-center bg-cover rounded-lg"> 
+                <Image src={post?.page_cover}  alt={post.title} fill  />
+            </div>
 
             </div>
 
