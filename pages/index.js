cc4ca1c6932e1e1dedd530de57aba1c1d23e920d
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 15:02:24 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Hero from '@/components/Hero/Home'
 import Pagination from '@/components/Pagination'
-import { getAllPosts, getPostBlocks } from '@/lib/notion'
+import { getAllPosts, getAllTagsFromPosts , getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
 import { register } from 'swiper/element/bundle'
 register()
+import FormattedDate from '@/components/Common/FormattedDate'
 
 export async function getStaticProps() {
   const posts = await getAllPosts({ onlyPost: true })
-
+  const tags = getAllTagsFromPosts(posts)
   const heros = await getAllPosts({ onlyHidden: true })
   const hero = heros.find((t) => t.slug === 'index')
 
@@ -31,25 +32,27 @@ export async function getStaticProps() {
       page: 1, // current page is 1
       postsToShow,
       showNext,
+      posts,
+      tags,
       blockMap
     },
     revalidate: 1
   }
 }
 
-const blog = ({ postsToShow, page, showNext, blockMap }) => {
+const blog = ({ postsToShow, page, showNext, blockMap, tags, posts,}) => {
   return ( <> 
  <div className='hidden md:block'>
-<swiper-container 
-grab-cursor="true" autoplay="true" autoplay-disable-on-interaction="true" speed="100" space-between="0" slides-per-view="3"
-  effect="coverflow" coverflow-effect-rotate="10" coverflow-effect-depth="500" coverflow-effect-slide-shadows="false" loop="true"
-  coverflow-effect-stretch="10" coverflow-effect-modifier="1" loop-additional-slides="2"
-  >      {postsToShow.map((post) => (<>
+      <swiper-container 
+        grab-cursor="true" autoplay="true" autoplay-disable-on-interaction="true" speed="100" space-between="0" slides-per-view="3"
+        effect="coverflow" coverflow-effect-rotate="10" coverflow-effect-depth="500" coverflow-effect-slide-shadows="false" loop="true"
+        coverflow-effect-stretch="10" coverflow-effect-modifier="1" loop-additional-slides="2"
+        >      {postsToShow.map((post) => (<>
         
        <swiper-slide key={post.id} post={post} index={postsToShow.indexOf(post)} > 
        <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
        <Image src={post?.page_cover} alt={post.title} width={640} height={480} />
-       <span class="mytext">{post.title} </span>
+       <span class="mytext">{post.title} <FormattedDate date={post.date} /></span>
        </Link>
        </swiper-slide>
        </>
@@ -60,7 +63,7 @@ grab-cursor="true" autoplay="true" autoplay-disable-on-interaction="true" speed=
 
     <div className='visible md:hidden '>
     <Container title={BLOG.title} description={BLOG.description}>
-      <Hero blockMap={blockMap} />
+      <Hero tags={tags} posts={posts} blockMap={blockMap} />
       {postsToShow.map((post) => (
        <BlogPost key={post.id} post={post} index={postsToShow.indexOf(post)} />
       ))}
