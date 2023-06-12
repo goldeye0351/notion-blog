958ba41a6a9411e4b6cab80b7cd939ed05a49d4a
Goldeye0351 <goldeye0351@gmail.com>
2023-06-12 12:10:28 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -16,12 +16,12 @@ const BlogPost = ({ index , post }) => {
          <div
                 id='blog-ID'
                 key={post.id}
-                className={`h-96 w-full md:flex justify-between  ${index % 2 === 1 ? 'flex-row-reverse ' : ''}
+                className={`h-56 w-full md:flex justify-between  ${index % 2 === 1 ? 'flex-row-reverse ' : ''}
                 overflow-hidden rounded-xl `}>
 
                 {/* 文字内容 */}
             <div className='relative my-auto mx-auto w-5/12 flex justify-center flex-col items-center  self-center  z-10'>
-                <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100'>{post.title}</h2>
+                <h2 className='text-lg md:text-xl font-medium mb-2 text-gray-100'>{post.title}</h2>
                 <span className='hidden md:block text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
                   <FormattedDate date={post.date} />
                 </span>
