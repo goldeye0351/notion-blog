5355ab8b084492ea71a5aff7021b005a84394465
Goldeye0351 <goldeye0351@gmail.com>
2023-06-17 18:40:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   description: '535251.xyz',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#212936', // use hex value, don't forget '#'
@@ -30,7 +30,7 @@ const BLOG = {
   defaultCover: '/mycover.jpg',
   socialLink: {
     twitter: 'https://twitter.com/',
-    github: 'https://github.com/',
+    github: 'https://github.com/goldeye0351',
     telegram: 'https://t.me/'
   },
   seo: {
