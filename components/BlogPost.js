251731cac62ede32c9f6b7a78dca77f444b1b9d5
Goldeye0351 <goldeye0351@gmail.com>
2023-06-19 14:48:19 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -20,9 +20,8 @@ const BlogPost = ({ index , post }) => {
         >
          <div
                          data-aos="fade-up"
-                         data-aos-offset="200"
-                         data-aos-delay="50"
-                         data-aos-duration="1000"
+                         data-aos-offset="100"
+                         data-aos-duration="500"
                          data-aos-easing="ease-in-out"
                          data-aos-once="false"
                          data-aos-anchor-placement="top-bottom"
