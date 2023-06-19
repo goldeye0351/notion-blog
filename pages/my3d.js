d494a171fec4bb527663f6530664241af876e97e
Goldeye0351 <goldeye0351@gmail.com>
2023-06-19 14:48:41 +0800

    Update my3d.js

--- pages/my3d.js
+++ pages/my3d.js
@@ -1,50 +1,103 @@
-import { getAllPosts, getAllTagsFromPosts } from '@/lib/notion'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { getAllPosts, getAllTagsFromPosts , getPostBlocks } from '@/lib/notion'
+import BLOG from '@/blog.config'
 import { register } from 'swiper/element/bundle'
 register()
-import Mswiper from '@/components/Myswiper/Mswiper'
-import { data } from '@/components/Paopao/data'
-import FluidAnimation from 'react-fluid-animation'
-import { darkBackground } from '@/blog.config'
+import FormattedDate from '@/components/Common/FormattedDate'
 
 
 export async function getStaticProps() {
-  const posts = await getAllPosts({ onlyNewsletter: false })
+  const posts = await getAllPosts({ onlyPost: true })
   const tags = getAllTagsFromPosts(posts)
+  const heros = await getAllPosts({ onlyHidden: true })
+  const hero = heros.find((t) => t.slug === 'index')
+
+  let blockMap
+  try {
+    blockMap = await getPostBlocks(hero.id)
+  } catch (err) {
+    console.error(err)
+    // return { props: { post: null, blockMap: null } }
+  }
+
+  const postsToShow = posts.slice(0, BLOG.postsPerPage)
+  const totalPosts = posts.length
+  const showNext = totalPosts > BLOG.postsPerPage
   return {
     props: {
+      page: 1, // current page is 1
+      postsToShow,
+      showNext,
+      posts,
       tags,
-      posts
+      blockMap
     },
     revalidate: 1
   }
 }
-
-export default function My3d( ) {
+const my3d = ({ postsToShow }) => {
   
   return <>
-  {/*
-<swiper-container 
+
+{/*  这里是swiper 插件 小屏隐藏  中等屏幕开始出现,  */}
+  <div className='hidden md:block '>
+      <swiper-container 
         grab-cursor="true" autoplay="true" autoplay-disable-on-interaction="true" speed="100" space-between="0" slides-per-view="3"
         effect="coverflow" coverflow-effect-rotate="10" coverflow-effect-depth="500" coverflow-effect-slide-shadows="false" loop="true"
         coverflow-effect-stretch="10" coverflow-effect-modifier="1" loop-additional-slides="2"
         > 
-{data.map((datap) => (
+        {postsToShow.map((post) => (<>
+        <swiper-slide key={post.id} post={post} index={postsToShow.indexOf(post)} > 
+          <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
+          {/* <Image src={post?.page_cover} alt={post.title} width={640} height={480} />  */}
 
-       <Mswiper key={datap.id} post={datap} className=" flex flex-col justify-center  items-center text-xl  bg-slate-300 dark:bg-slate-600  rounded-3xl " />
+          <div className=' max-w-[640px] max-h-[480px] flex flex-col justify-between'>  
+              <Image src={post?.page_cover} alt={post.title} width={640} height={480} 
+              className='rounded-3xl  static 
+              invert dark:invert-0
+              '/>
+              <div className='absolute flex flex-col justify-between  p-8  text-xl '>{post.title} 
+              <div className=' hidden lg:block text-sm '>{post.summary}</div>
+              </div>
+
+              <div className='absolute bottom-0 p-8 '><FormattedDate date={post.date} /></div>
+          </div>
+          </Link>
+        </swiper-slide>
+        </>
+        ))}
+      </swiper-container>
+  </div>
+
+   {/*  小屏开始隐藏  */}
+  <div className='visible md:hidden '>
+    <div className='max-w-[95VW] justify-center content-center items-center mx-auto' >
+      <swiper-container navigation="true" pagination="true" scrollbar="true" loop="true" grab-cursor="true" > 
+        {postsToShow.map((post) => (
+        <>
+        <swiper-slide key={post.id} post={post} index={postsToShow.indexOf(post)} > 
+          <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
+          {/* <Image src={post?.page_cover} alt={post.title} width={640} height={480} />  */}
       
-      ))}
-</swiper-container> */}
-<div className=' h-screen w-screen flex flex-col justify-center items-center content-center'>
-<div className='h-[300px] w-[600px] flex flex-col justify-center content-center items-center overflow-hidden rounded-full' >
-  <div className=' absolute text-center text-red-200 z-10 '> asfasdfasdfasf</div >
-<FluidAnimation
-        style={{ radius:990,   height: '300px', width:'610px' }}
-      />
-</div>
-</div>
+          <div key={post.id} className=' max-w-[calc(100vw-10rem)] min-h-[calc(100vh-14rem)]  flex flex-col justify-between'>  
+              <Image src={post?.page_cover} alt={post.title} fill 
+              className='rounded-3xl  static 
+              invert dark:invert-0
+              '/>
+              <div className='absolute flex flex-col justify-between  p-8  text-xl '>{post.title} 
+              <div className=' text-sm '>{post.summary}</div>
+              </div>
+              <div className='absolute bottom-0 p-8 '><FormattedDate date={post.date} /></div>
+          </div>
+          </Link>
+        </swiper-slide>
+        </>
+        ))}
+      </swiper-container>
+    </div>
+  </div>
 
-
-
-
-</>
-}
+</>}
+export default my3d
