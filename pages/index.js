17f7b5f280b2defa157deb74e8d8d9fa004638ec
Goldeye0351 <goldeye0351@gmail.com>
2023-06-20 20:35:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Container from '@/components/Container'
-import BlogPost from '@/components/BlogPost'
+
 import Image from 'next/image'
 import Link from 'next/link'
-import Hero from '@/components/Hero/Home'
-import Pagination from '@/components/Pagination'
 import { getAllPosts, getAllTagsFromPosts , getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
 import { register } from 'swiper/element/bundle'
 register()
 import FormattedDate from '@/components/Common/FormattedDate'
+
 
 export async function getStaticProps() {
   const posts = await getAllPosts({ onlyPost: true })
@@ -39,57 +37,67 @@ export async function getStaticProps() {
     revalidate: 1
   }
 }
+const my3d = ({ postsToShow }) => {
+  
+  return <>
 
-const blog = ({ postsToShow, page, showNext, blockMap, tags, posts,}) => {
-  return ( <> 
-  {/*  这里是HERO 小屏中屏出现, 大屏隐藏  */}
-  <Hero tags={tags} posts={posts} blockMap={blockMap} />
-
-  {/*  这里是swiper 插件 小屏隐藏  中等屏幕开始出现,  */}
- <div className='hidden md:block'>
+{/*  这里是swiper插件 很多图的模式  小屏隐藏  中等屏幕开始出现,  */}
+  <div className='hidden md:block '>
       <swiper-container 
         grab-cursor="true" autoplay="true" autoplay-disable-on-interaction="true" speed="100" space-between="0" slides-per-view="3"
         effect="coverflow" coverflow-effect-rotate="10" coverflow-effect-depth="500" coverflow-effect-slide-shadows="false" loop="true"
         coverflow-effect-stretch="10" coverflow-effect-modifier="1" loop-additional-slides="2"
         > 
         {postsToShow.map((post) => (<>
-        
-  <swiper-slide key={post.id} post={post} index={postsToShow.indexOf(post)} > 
-    <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
-    {/* <Image src={post?.page_cover} alt={post.title} width={640} height={480} />  */}
+        <swiper-slide key={post.id} post={post} index={postsToShow.indexOf(post)} > 
+          <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
+          {/* <Image src={post?.page_cover} alt={post.title} width={640} height={480} />  */}
 
-    <div className=' max-w-[640px] max-h-[480px] min-w-[480px] min-h-[360px] flex flex-col justify-between'>  
-        <Image src={post?.page_cover} alt={post.title} width={640} height={480} 
-        className='rounded-3xl  static 
-        invert dark:invert-0
-        '/>
-        <div className='absolute flex flex-col justify-between  p-8  text-xl '>{post.title} 
-        <div className=' hidden lg:block text-sm '>{post.summary}</div>
-        </div>
+          <div className=' max-w-[800px] max-h-[600px] min-w-[480px] min-h-[360px] flex flex-col justify-between'>  
+              <Image src={post?.page_cover} alt={post.title} width={720} height={540} 
+              className='rounded-3xl  static 
+              
+              '/>
+              <div className='absolute flex flex-col justify-between  p-8  text-xl '>{post.title} 
+              <div className=' hidden lg:block text-sm '>{post.summary}</div>
+              </div>
 
-        <div className='absolute bottom-0 p-8 '><FormattedDate date={post.date} /></div>
-    </div>
-
-    </Link>
-  </swiper-slide>
-       </>
-      ))}
+              <div className='absolute bottom-0 p-8 '><FormattedDate date={post.date} /></div>
+          </div>
+          </Link>
+        </swiper-slide>
+        </>
+        ))}
       </swiper-container>
-      
   </div>
-   {/*  中屏开始隐藏  */}
-  <div className='visible md:hidden '>
-    <Container title={BLOG.title} description={BLOG.description}>
-      
-      {postsToShow.map((post) => (
-       <BlogPost key={post.id} post={post} index={postsToShow.indexOf(post)} />
-      ))}
-      {/* {showNext && <Pagination page={page} showNext={showNext} />} */}
-    </Container>
-  </div>
-    
-    </>
-  )
-}
 
-export default blog
+   {/*  单图 模式 中屏开始隐藏  */}
+  <div className='visible md:hidden '>
+    <div className='max-w-[95VW] justify-center content-center items-center mx-auto' >
+      <swiper-container navigation="true" pagination="true" scrollbar="true" loop="true" grab-cursor="true" > 
+        {postsToShow.map((post) => (
+        <>
+        <swiper-slide key={post.id} post={post} index={postsToShow.indexOf(post)} > 
+          <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
+          {/* <Image src={post?.page_cover} alt={post.title} width={640} height={480} />  */}
+      
+          <div key={post.id} className=' max-w-[calc(100vw-10rem)] min-h-[calc(100vh-14rem)]  flex flex-col justify-between'>  
+              <Image src={post?.page_cover} alt={post.title} fill 
+              className='rounded-3xl  static 
+              invert dark:invert-0
+              '/>
+              <div className='absolute flex flex-col justify-between  p-8  text-xl '>{post.title} 
+              <div className=' text-sm '>{post.summary}</div>
+              </div>
+              <div className='absolute bottom-0 p-8 '><FormattedDate date={post.date} /></div>
+          </div>
+          </Link>
+        </swiper-slide>
+        </>
+        ))}
+      </swiper-container>
+    </div>
+  </div>
+
+</>}
+export default my3d
