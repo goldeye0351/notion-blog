89d79eece3d182c5138ace0b7e89cde89e6b713f
Goldeye0351 <goldeye0351@gmail.com>
2023-06-22 22:54:05 +0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import FluidAnimation from "react-fluid-animation"
 const test = ( ) => {
   
   return <>
-<div className=' w-screen h-screen '>
+<div className=' w-screen h-[80vh]'>
 <FluidAnimation />
 </div>
   
