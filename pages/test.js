f5528dee02d61f641b260986b28ab5d552db3b5f
Goldeye0351 <goldeye0351@gmail.com>
2023-06-26 16:58:09 +0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,8 @@
-import FluidAnimation from "react-fluid-animation"
 
 const test = ( ) => {
   
   return <>
-<div className=' w-screen h-[80vh]'>
-<FluidAnimation />
-</div>
+
   
 </>}
 export default test
