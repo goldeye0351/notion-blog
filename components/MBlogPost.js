0531aab6855f7863aa69363b386182f544503a7f
Goldeye0351 <goldeye0351@gmail.com>
2023-06-27 18:49:48 +0800

    Update MBlogPost.js

--- components/MBlogPost.js
+++ components/MBlogPost.js
@@ -8,28 +8,49 @@ import React, { useEffect } from 'react'
 const BlogPost = ({ index , post,className }) => {
   
 
-  return (
-    <motion.div className={className} >
-      <Link passHref href={`${BLOG.path}/${post.slug}`} >
-      <article
+  return (    
+<motion.div>
+      <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
+        <article
           key={post.id}
-          className='group mb-5 md:mb-8 cursor-pointer rounded-xl p-5 mx-8 my-8 w-full h-full '
+          className='group flex flex-col overflow-hidden relative mb-5 md:mb-8 cursor-pointer rounded-xl p-5'
         >
-          
-
+          <Image
+            fill
+            alt={`${post.title}`}
+            src={post?.page_cover}
+            className='w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200'
+          />
+          <div className='hidden md:block md-cover absolute inset-0'></div>
+          <div className='md:hidden sm-cover absolute inset-0'></div>
           <div className='relative mt-auto'>
             <header className='flex flex-col justify-between md:flex-row md:items-baseline'>
-              <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100'>{post.title}</h2>
+              <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100'>
+                 <span className="  ">{post.title}</span>
+                 
+                 <svg  className=" w-40 h-10 "  >
+                      <rect className="myshape "    />
+                 </svg>
+                 <span className=" mysvgtext hover:scale-110 duration-500    ">{post.tags}</span>
+        
+
+               </h2>
+
+
+                    
+
               <span className='text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
-                <FormattedDate date={post.date} />
+                <FormattedDate className="mytext2" date={post.date} />
               </span>
             </header>
-            <p className='font-light block leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
+            <p className='font-light hidden md:block leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
             {/* w-4/5  */}
           </div>
         </article>
       </Link>
     </motion.div>
+
+
   )
 }
 
