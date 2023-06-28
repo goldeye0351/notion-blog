a5716864aa26a2bb974efee8da99b0edc146e98d
Goldeye0351 <goldeye0351@gmail.com>
2023-06-28 17:59:21 +0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -2,7 +2,12 @@
 const test = ( ) => {
   
   return <>
-
+<div className="myshape" height="40" width="150" >
+    <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
+      <rect height="40" width="150" />
+    </svg>
+    <div class="mymenutext">       123123123123      </div>
+  </div>
   
 </>}
 export default test
