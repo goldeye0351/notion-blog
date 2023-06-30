9e8bf0ac709ed8169e61ec9965b227b572514d06
Goldeye0351 <goldeye0351@gmail.com>
2023-06-30 09:36:59 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -24,16 +24,31 @@ class MyDocument extends Document {
             title='RSS 2.0'
             href='/feed'
           ></link>
-
+          {BLOG.appearance === 'auto' ? (
+            <>
+              <meta
+                name='theme-color'
+                content={BLOG.lightBackground}
+                media='(prefers-color-scheme: light)'
+              />
               <meta
                 name='theme-color'
                 content={BLOG.darkBackground}
                 media='(prefers-color-scheme: dark)'
               />
-
-
+            </>
+          ) : (
+            <meta
+              name='theme-color'
+              content={
+                BLOG.appearance === 'dark'
+                  ? BLOG.darkBackground
+                  : BLOG.lightBackground
+              }
+            />
+          )}
         </Head>
-        <body className='bg-night'>
+        <body className='bg-day dark:bg-night'>
           <Main />
           <NextScript />
         </body>
