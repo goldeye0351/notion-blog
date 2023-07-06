7cbd1010c96d7ed116b2a6d5a3441182007ee647
Goldeye0351 <goldeye0351@gmail.com>
2023-07-06 18:44:53 +0800

    Create Data.js

---
+++ components/Data.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import My1IMG from "@/public/1.png"
+import My2IMG from "@/public/2.png"
+import My3IMG from "@/public/3.png"
+import My4IMG from "@/public/4.png"
+import My5IMG from "@/public/mycover.jpg"
+
+export const pngdata = [
+{text:"第1个",image:<Image src={My1IMG} alt="" />},
+{text:"第2个",image:<Image src={My2IMG} alt="" />},
+{text:"第3个",image:<Image src={My3IMG} alt="" />},
+{text:"第4个",image:<Image src={My4IMG} alt="" />},
+{text:"第5个",image:<Image src={My5IMG} alt="" />},
+{text:"第1个",image:<Image src={My1IMG} alt="" />},
+{text:"第2个",image:<Image src={My2IMG} alt="" />},
+{text:"第3个",image:<Image src={My3IMG} alt="" />},
+{text:"第4个",image:<Image src={My4IMG} alt="" />},
+{text:"第5个",image:<Image src={My5IMG} alt="" />},
+{text:"第1个",image:<Image src={My1IMG} alt="" />},
+{text:"第2个",image:<Image src={My2IMG} alt="" />},
+{text:"第3个",image:<Image src={My3IMG} alt="" />},
+{text:"第4个",image:<Image src={My4IMG} alt="" />},
+{text:"第5个",image:<Image src={My5IMG} alt="" />},
+{text:"第1个",image:<Image src={My1IMG} alt="" />},
+{text:"第2个",image:<Image src={My2IMG} alt="" />},
+{text:"第3个",image:<Image src={My3IMG} alt="" />},
+{text:"第4个",image:<Image src={My4IMG} alt="" />},
+{text:"第5个",image:<Image src={My5IMG} alt="" />},
+  ];
