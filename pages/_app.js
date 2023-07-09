4324defe1e4ff508a4556a69be5590e2d4a657ae
Goldeye0351 <goldeye0351@gmail.com>
2023-07-09 15:52:01 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -69,7 +69,7 @@ function MyApp({ Component, pageProps }) {
         />
         <TransitionEffect>
             <div
-              className={`min-h-[calc(100vh-13rem)] md:min-h-[calc(100vh-17rem)] ${
+              className={`min-h-[calc(100vh-19rem)] md:min-h-[calc(100vh-17rem)] ${
                 BLOG.font === 'serif' ? 'font-serif' : 'font-sans'
               }`}
             >
