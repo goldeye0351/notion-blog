40731af5f2517202124ccba6874e87b40a7b893f
Goldeye0351 <goldeye0351@gmail.com>
2023-07-11 15:37:48 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Logoimg from "@/public/favicon.png"
 import { getAllPosts, getAllTagsFromPosts,getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
 import NotionRenderer from '@/components/Post/NotionRenderer'
@@ -22,19 +23,20 @@ export async function getStaticProps() {
     props: {
       posts,
       tags,
+      hero,
       blockMap
     },
     revalidate: 1
   }
 }
-const About = ({ posts,blockMap }) => {
+const About = ({ blockMap,hero }) => {
     return (
 <div title={BLOG.about} description={BLOG.description} className='about' >
   <div className="flex flex-col justify-center content-center items-center pb-12  space-y-16">
     
      <Tilt 
-              className="my3d shadow-2xl shadow-gray-500 rounded-xl  max-w-[80VW] 
-              bg-[url('../public/images/mac.jpg')]
+              className="my3d shadow-lg shadow-indigo-500 rounded-xl  max-w-[80VW] 
+              bg-[url('../public/mycover.jpg')]
               bg-cover
               "
               perspective={1500}
@@ -46,19 +48,13 @@ const About = ({ posts,blockMap }) => {
               scale={1.02}
             >
                
-              <div className="my3din flex flex-col justify-center items-center flex-wrap p-12">
-                {/*<Image src={LOGOIMG} alt={BLOG.title}  height={180}  />*/}
+              <div className="my3din flex flex-col justify-center items-center flex-wrap p-12  ">
+                {/*<Image  src={hero?.page_cover} alt={BLOG.title} fill className=' rounded-3xl  '/>*/}
+                <Image  src={Logoimg}  alt='Logo'  width={100}  height={100}  className=' animate-bounce  '/>
                 <NotionRenderer blockMap={blockMap} frontMatter={{}} subPageTitle={null} />
               </div>
               
     </Tilt>
- {/*
-    <div className='h-[300px] w-[600px] max-w-[80VW] flex flex-col justify-center content-center items-center overflow-hidden rounded-full' >
-    <FluidAnimation
-            style={{ radius:990,   height: '300px', width:'600px'  }}
-          />
-    </div>
-    */}
  </div>
    <SupaComments />
 </div>
