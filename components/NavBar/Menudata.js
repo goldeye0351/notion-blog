6ba07b957f6c11ba020e533263851e36fdf974ad
Goldeye0351 <goldeye0351@gmail.com>
2023-07-12 17:22:58 +0800

    Update Menudata.js

--- components/NavBar/Menudata.js
+++ components/NavBar/Menudata.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import BLOG from '@/blog.config'
-import {  HomeIcon,  SearchIcon, CloudIcon,CloudDownloadIcon ,CloudUploadIcon,FingerPrintIcon,EyeOffIcon,EyeIcon, MenuIcon,  UserIcon,  UsersIcon,  MailIcon,  ClipboardListIcon} from '@heroicons/react/outline'
+import {  HomeIcon,  SearchIcon, GlobeIcon,GlobeAltIconicon, CloudIcon,CloudDownloadIcon ,CloudUploadIcon,FingerPrintIcon,EyeOffIcon,EyeIcon, MenuIcon,  UserIcon,  UsersIcon,  MailIcon,  ClipboardListIcon} from '@heroicons/react/outline'
 
 export const links = [
     {
@@ -17,37 +17,43 @@ export const links = [
     },
     {
       id: 2,
+      name: 'Travel',
+      to: '/travel',
+      icon: <GlobeIcon className='inline-block mb-1 h-5 w-5' />,
+    },
+    {
+      id: 3,
       name: 'Search',
       to: '/search',
       icon: <SearchIcon className='inline-block mb-1 h-5 w-5' />,
     },
     {
-      id: 3,
+      id: 4,
       name: 'About',
       to: '',
       icon: <UserIcon className='inline-block mb-1 h-5 w-5' />,
       submenu:
       [
         {
-          id: 4,
+          id: 5,
           name: 'Me',
           to: '/about',
           icon: <UserIcon className='inline-block mb-1 h-5 w-5' />,
         },
         {
-          id: 5,
+          id: 6,
           name: 'Contact',
           to: '/contact',
           icon: <MailIcon className='inline-block mb-1 h-5 w-5' />,
         },
         {
-          id: 6,
+          id: 7,
           name: 'Friends',
           to: '/friends',
           icon: <UsersIcon className='inline-block mb-1 h-5 w-5' />,
         },
         {
-          id: 7,
+          id: 8,
           name: 'Privacy',
           to: '/privacy',
           icon: <EyeOffIcon className='inline-block mb-1 h-5 w-5' />,
