e7743688da3db46b8c37fc4a24e10df9224c688e
Goldeye0351 <goldeye0351@gmail.com>
2023-07-12 16:59:49 +0800

    Update Menudata.js

--- components/NavBar/Menudata.js
+++ components/NavBar/Menudata.js
@@ -5,71 +5,50 @@ import {  HomeIcon,  SearchIcon, CloudIcon,CloudDownloadIcon ,CloudUploadIcon,Fi
 export const links = [
     {
       id: 0,
-      name: '首页',
+      name: 'Home',
       to: BLOG.path || '/',
       icon: <HomeIcon className='inline-block mb-1 h-5 w-5' />,
     },
     {
       id: 1,
-      name: '博客',
+      name: 'Blog',
       to: '/blog',
       icon: <ClipboardListIcon className='inline-block mb-1 h-5 w-5' />,
     },
     {
       id: 2,
-      name: '建站指引',
-      to: '',
-      icon: <CloudIcon className='inline-block mb-1 h-5 w-5' />,
-      submenu:
-      [
-        {
-          id: 3,
-          name:'建站指引',
-          to: '/jz01',
-          icon: <CloudDownloadIcon className='inline-block mb-1 h-5 w-5' />,
-        },
-        {
-          id: 4,
-          name: '进阶说明',
-          to: '/jz02',
-          icon: <CloudUploadIcon className='inline-block mb-1 h-5 w-5' />,
-        }
-      ]
-    },
-    {
-      id: 5,
-      name: '搜索',
+      name: 'Search',
       to: '/search',
       icon: <SearchIcon className='inline-block mb-1 h-5 w-5' />,
     },
     {
-      id: 6,
-      name: '关于',
+      id: 3,
+      name: 'About',
       to: '',
       icon: <UserIcon className='inline-block mb-1 h-5 w-5' />,
       submenu:
       [
         {
-          id: 7,
+          id: 4,
           name: 'Me',
           to: '/about',
           icon: <UserIcon className='inline-block mb-1 h-5 w-5' />,
         },
         {
-          id: 8,
-          name: '私信',
+          id: 5,
+          name: 'Contact',
           to: '/contact',
           icon: <MailIcon className='inline-block mb-1 h-5 w-5' />,
         },
         {
-          id: 9,
-          name: '友链',
+          id: 6,
+          name: 'Friends',
           to: '/friends',
           icon: <UsersIcon className='inline-block mb-1 h-5 w-5' />,
         },
         {
-          id: 10,
-          name: '隐私',
+          id: 7,
+          name: 'Privacy',
           to: '/privacy',
           icon: <EyeOffIcon className='inline-block mb-1 h-5 w-5' />,
         }
