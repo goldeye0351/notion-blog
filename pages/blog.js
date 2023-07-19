63c596b72099ab0c0075b7aa9c9e9b5f69105bbe
Goldeye0351 <goldeye0351@gmail.com>
2023-07-19 17:04:07 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,51 +1,108 @@
 import Container from '@/components/Container'
 import BlogPost from '@/components/MBlogPost'
-import Hero from '@/components/Hero/Home'
-import { getAllPosts, getAllTagsFromPosts, getPostBlocks } from '@/lib/notion'
+import { getAllPosts, getAllTagsFromPosts } from '@/lib/notion'
+import PropTypes from 'prop-types'
 import BLOG from '@/blog.config'
-import Pagination from '@/components/Pagination'
 import Link from 'next/link'
 import Image from 'next/image'
 import { register } from 'swiper/element/bundle'
 register()
+import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { PlayIcon,ShareIcon } from "@heroicons/react/outline"
 
-export async function getStaticProps() {
+const variants = {
+  in: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 0.25,
+      delay: 0.25
+    }
+  },
+  out: {
+    opacity: 0,
+    scale: 0.9,
+    y: 50,
+    transition: {
+      duration: 0.25
+    }
+  }
+}
+export async function getStaticProps({name}) {
   const posts = await getAllPosts({ onlyPost: true })
   const tags = getAllTagsFromPosts(posts)
-  const heros = await getAllPosts({ onlyHidden: true })
-  const hero = heros.find((t) => t.slug === 'index')
 
-  let blockMap
-  try {
-    blockMap = await getPostBlocks(hero.id)
-  } catch (err) {
-    console.error(err)
-    // return { props: { post: null, blockMap: null } }
-  }
-
-  const postsToShow = posts.slice(0, BLOG.postsPerPage)
-  const totalPosts = posts.length
-  const showNext = totalPosts > BLOG.postsPerPage
   return {
     props: {
-      page: 1, // current page is 1
-      postsToShow,
-      showNext,
       posts,
       tags,
-      hero,
-      blockMap
+
     },
     revalidate: 1
   }
 }
 
-const blog = ({ postsToShow, page, showNext, tags, posts, blockMap,hero }) => {
+const blog = ({ tags, posts, currentTag }) => {
+  const [name, setName] = useState('')
+  const filteredBlogPosts = posts.filter(
+    (post) => post && post.tags && post.tags.includes(name)
+  )
+ const deftag = name === "" ? posts : filteredBlogPosts 
+
   return (
     <Container title={BLOG.title} description={BLOG.description}>
-      
-      {/*<Hero  tags={tags} heropost={hero} blockMap={blockMap} />*/}
+      <div id="allswiper" className=" mt-3 w-full min-h-[60vh] max-h-[60vh]   rounded-2xl relative  overflow-clip  ">
+            <swiper-container loop="true" autoplay="true" slides-per-view="auto" autoplay-delay="3000" 
+            navigation="true" parallax="true" pagination="true" scrollbar="true" grab-cursor="true" 
+            > 
+            
+            {deftag.map((png) => (<>
+                <swiper-slide key={png.id}  >
+                    <Link passHref href={`${BLOG.path}/${png.slug}`} scroll={false}  
+                    className=" h-[60VH] min-h-[60vh] max-h-[60vh] flex flex-col justify-center ">
+                        {png?.page_cover}
+                        <Image key={png.id} src={png?.page_cover} alt={png.title} fill className='rounded-3xl  static '/>
+                        <div data-swiper-parallax="1000" data-swiper-parallax-scale="0.05" data-swiper-parallax-duration="600"
+                        className=" absolute top-[0%] flex justify-center right-[0%]  p-3 text-xl rounded-lg bg-white/80 dark:bg-black/50"   >
+                        {png.title}     
+                        </div>
+                        <div data-swiper-parallax="-1000" data-swiper-parallax-scale="0.05" data-swiper-parallax-duration="1000"
+                        className=" absolute bottom-0 flex justify-center left-[0%] mx-auto p-3 rounded-lg bg-white/80 dark:bg-black/50"   >
+                        {png.summary}     
+                        </div>
+                    </Link>
+                </swiper-slide>
+            </>))}
+            </swiper-container>
+      </div>  
+      <div id="trreetags" className='tag-container my-3'>
+        <div className=' flex flex-row flex-grow  w-full items-center justify-between  space-x-2 '>
+          {Object.keys(tags).map((key) => {
+            console.log("TAG",name)
+            return (
+              <button
+                key={key}
+                onClick={() => { setName(key) }} 
+                className={` group px-3 w-1/3 hover:w-1/2 h-16 flex flex-row justify-between content-center items-center   ${ Object.keys(tags).indexOf(key)% 3 === 0 ? 'cai1 ' :Object.keys(tags).indexOf(key)% 3  === 1 ? 'cai2 ' : 'cai3'}
+                overflow-hidden rounded-xl   hover:scale-110 duration-500   m-1 font-medium  whitespace-nowrap  hover:bg-gray-400 dark:hover:bg-gray-600`}
+              >
+               {`${key} (${tags[key]})`}
+                <ShareIcon 
+                className={`inline-block scale-150  h-16 w-16 -rotate-45 opacity-50 group-hover:rotate-0 group-hover:opacity-100 group-hover:scale-105     ${ Object.keys(tags).indexOf(key)% 2 === 1 ? 'hidden ' : ''}
+                duration-500 `}/>
+                <PlayIcon 
+                className={`inline-block scale-150  h-16 w-16 -rotate-45 opacity-50 group-hover:rotate-0 group-hover:opacity-100 group-hover:scale-105      ${ Object.keys(tags).indexOf(key)% 2 === 1 ? ' ' : 'hidden'}
+                duration-500 `}/>
+              </button>
+            )
+          })}
+        </div>
+      </div>
 
+
+{/*
 <div className='flex flex-row space-x-3'>
     <div className=' md:w-4/5 w-full rounded-lg mb-5 md:mb-8 flex justify-center content-center items-center mx-auto  ' >
           <swiper-container loop="true" autoplay="true" slides-per-view="1" autoplay-delay="1500"  navigation="true" pagination="true" scrollbar="true" grab-cursor="true" 
@@ -91,13 +148,32 @@ const blog = ({ postsToShow, page, showNext, tags, posts, blockMap,hero }) => {
        </div>
 
     </div> 
-</div>
-      {posts.map((post) => (
-        <BlogPost key={post.id} post={post} index={postsToShow.indexOf(post)} />
+</div> */}
+    <div id="mainmotionblog" >
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter
+      >
+        <motion.div
+          key={name}
+          variants={variants}
+          animate='in'
+          initial='out'
+          exit='out'
+        >
+      {deftag.map((post) => (
+        <BlogPost key={post.id} post={post} />
       ))}
-      {showNext && <Pagination page={page} showNext={showNext} />}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+
     </Container>
   )
 }
-
+blog.propTypes = {
+  posts: PropTypes.array.isRequired,
+  tags: PropTypes.object.isRequired,
+  currentTag: PropTypes.string
+}
 export default blog
