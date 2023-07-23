6b47fbc5120f3d3607fe08add60030891c98d96a
Goldeye0351 <goldeye0351@gmail.com>
2023-07-23 23:08:14 +0800

    Update timeline.js

--- pages/timeline.js
+++ pages/timeline.js
@@ -17,7 +17,7 @@ const timeline = ( {posts}) => {
   
   return     <Container title={BLOG.title} description={BLOG.description}>
 
-<div  className="  mx-auto  flex flex-col justify-center items-center  border-gray-500 dark:border-gray-300   " >
+<div id="dapin" className=" hidden md:flex mx-auto flex-col justify-center items-center" >
     <div className="h-full w-1  relative  border-gray-500 dark:border-gray-300 border-2 flex flex-col py-16 "  >
     {posts.map((post) => (<>
         <div key={post.id}              
@@ -34,6 +34,28 @@ const timeline = ( {posts}) => {
                     <div className="text-xl  "> {post.title}</div>
                     <div className="text-sm opacity-80 "><FormattedDate date={post.date} /></div>
                     <div className=" -translate-x-96 opacity-0 group-hover:translate-x-0 group-hover:opacity-100  duration-500  "> {post.summary}</div>
+                </div>        
+        </div>
+    </>))}
+    </div>
+</div>
+
+<div id="xioapin" className=" md:hidden mx-auto  flex flex-col justify-start items-start" >
+    <div className="h-full w-1  relative  border-gray-500 dark:border-gray-300 border-2 flex flex-col"  >
+    {posts.map((post) => (<>
+        <div key={post.id}              
+             id="timeline"
+             className=" w-[90VW]  flex flex-row  my-3 first:mt-6 last:mb-6
+             
+             ">   
+                < Mytesticon index ={posts.indexOf(post)% 6} />
+
+                <div 
+                id="timeline-panel"
+                className="group   mx-6 border-2 border-gray-500 dark:border-gray-300 rounded-xl  flex flex-col w-full p-3 bg-gray-300 dark:bg-gray-600">  
+                    <div className="text-xl  "> {post.title}</div>
+                    <div className="text-sm opacity-80 "><FormattedDate date={post.date} /></div>
+                    <div className=""> {post.summary}</div>
                 </div>        
         </div>
     </>))}
