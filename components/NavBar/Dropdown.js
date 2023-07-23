f23ea7ec7cd119c446f00a49c8977f768f0fc9aa
Goldeye0351 <goldeye0351@gmail.com>
2023-07-23 13:30:29 +0800

    Update Dropdown.js

--- components/NavBar/Dropdown.js
+++ components/NavBar/Dropdown.js
@@ -1,14 +1,14 @@
 import MenuItems from './MenuItems';
-const Dropdown = ({ submenus, dropdown, className}) => {
+const Dropdown = ({ submenus, dropdown}) => {
   const dropdownClass = '';
   return (
     <ul
-      className={`dropdown ${dropdownClass} ${
-        dropdown ? 'show' : ''
-      }`}
+      className={` dropdown ${dropdownClass} ${dropdown ? 'show' : ''}
+      bg-gray-300 dark:bg-gray-600  p-1 
+      `}
     >
       {submenus.map((submenu, index) => (
-        <MenuItems className={className}
+        <MenuItems 
           items={submenu}
           key={index}
         />
