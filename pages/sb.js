b7a2aee7f348c0b4dafa0093f3d56cff45857634
Goldeye0351 <goldeye0351@gmail.com>
2023-07-26 19:41:20 +0800

    Update sb.js

--- pages/sb.js
+++ pages/sb.js
@@ -2,34 +2,42 @@
 import BubbleUI from "@/components/Myswiper/Bb";
 import { Sbdata } from "@/components/Sbdata";
 import { pngdata } from "@/components/Data";
+import Typed from "typed.js";
+import { useEffect } from "react";
 
 export default function Iwatch(props) {
-	
-	const diyizu = pngdata.slice(0, 12)
-	const options = {
-		size: 180,
-		minSize: 50,
-		gutter: 8,
-		provideProps: true,
-		numCols: 8,
-		fringeWidth: 160,
-		yRadius: 130,
-		xRadius: 220,
-		cornerRadius: 50,
-		showGuides: false,
-		compact: true,
-		gravitation: 5
-	}
+
   
 	return (<>
-    <BubbleUI 
-        options={options}
-        className={"myBubbleUI h-[100VH]"}
-      >
-	   {/*{diyizu.map((data, i) => (<div>{data.image}</div>))} */}
-	   {Sbdata.slice(0,12)}
-      </BubbleUI>  
+	<div className=" flex mx-auto justify-center "> 
+		
+        <div className=' w-96  bg-slate-600  relative  overflow-scroll flex flex-row  '> 
+			< div id="m" className=" bg-gray-600 w-3/5 flex flex-col relative "> 
+				<div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div>
+				<div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div>
 
+				<div className=" ">11111111111</div>
+			</div>
+
+			< div id="s" className=" relative  bg-slate-500 w-2/5 flex  flex-wrap   flex-grow flex-col    ">
+					< div id="s1" className=" relative  bg-black w-full h-56 rounded-full my-8  ">
+						111111
+   					</div>
+					< div id="s2" className="sticky top-0 bottom-0  w-full   ">
+						
+						<div className=" h-16 p-6 my-8 rounded-xl  bg-red-300 ">
+							2222
+						</div>
+
+						< div id="s3" className=" rounded-full  bg-black w-full h-56 my-8 p-6 ">
+						5555
+   					    </div>
+   					</div>
+
+			</div>
+
+        </div>
+	</div>
 	</>
 		
 	)
