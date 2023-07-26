4d368ce91960cdeab51fdca0a276da46403413af
Goldeye0351 <goldeye0351@gmail.com>
2023-07-26 17:39:10 +0800

    Update Content.js

--- components/Post/Content.js
+++ components/Post/Content.js
@@ -1,32 +1,39 @@
 import BLOG from '@/blog.config'
 import PropTypes from 'prop-types'
-import Link from 'next/link'
 import FormattedDate from '@/components/Common/FormattedDate'
 import TagItem from '@/components/Common/TagItem'
 import NotionRenderer from '@/components/Post/NotionRenderer'
 import {EyeIcon} from '@heroicons/react/outline'
-import { ChevronLeftIcon } from '@heroicons/react/outline'
+import Typed from "typed.js";
+import { useEffect } from "react";
 
 export default function Content (props) {
-  const { frontMatter, blockMap, pageTitle } = props
+  const { frontMatter, blockMap, pageTitle,prev,next } = props
+  
+  useEffect(() => {
+		new Typed('#typed', {
+				strings: [frontMatter.summary],
+				typeSpeed: 50,
+				backSpeed: 50,
+				backDelay: 100,  
+				smartBackspace: true,
+				showCursor: true,
+				loop: false,
+				loopCount: 3
+			  })})
 
   return (
     <article className='flex-none md:overflow-x-visible overflow-x-scroll w-full max-w-5xl '>
-      {pageTitle && (
-        <Link
-          passHref
-          href={`${BLOG.path}/${frontMatter.slug}`}
-          scroll={false}
-          className='block md:-ml-6 mb-2 text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300'
-        >
-          <ChevronLeftIcon className='inline-block mb-1 h-5 w-5' />
-          <span className='m-1'>{frontMatter.title}</span>
-        </Link>
-      )}
+
       <h1 className='font-bold text-3xl text-black dark:text-white'>
         {pageTitle ? pageTitle : frontMatter.title}
       </h1>
-      {frontMatter.type[0] !== 'Page' && (
+
+      <div className=' text-black dark:text-white bg-gray-300  dark:bg-gray-600/50  ring-green-300/50 ring-2 p-3 m-8 rounded-xl'>
+        AI摘要:<span className='inline-block    ' id='typed' /> 
+
+      </div>
+
         <nav className='flex mt-5 mb-10 items-start text-gray-500 dark:text-gray-400'>
           <div className='mr-2 mb-4 md:ml-0'>
             <FormattedDate date={frontMatter.date} />
@@ -38,14 +45,8 @@ export default function Content (props) {
               ))}
             </div>
           )}
-          <div>                
-              <EyeIcon className='inline-block  w-6 h-6 '/>
-              <span id="busuanzi_container_page_pv" className='  inline-block '><span id="busuanzi_value_page_pv"></span></span> 
-          </div>
-
-
         </nav>
-      )}
+
       <div className="-mt-4 relative">
         <NotionRenderer
           blockMap={blockMap}
@@ -53,6 +54,7 @@ export default function Content (props) {
           {...props}
         />
       </div>
+
     </article>
   )
 }
