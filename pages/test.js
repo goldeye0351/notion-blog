f302a8340d8ee759b9ffb86098537d757c31d8c7
Goldeye0351 <goldeye0351@gmail.com>
2023-07-26 11:01:15 +0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -4,19 +4,21 @@ import HeroLeft from '@/components/Hero/Heroleft'
 import HeroRight from '@/components/Hero/HeroRight'
 import CardTags from '@/components/Card/CardTag'
 import Banner from '@/components/Hero/Banner'
+import Script from 'next/script'
+
 
 export async function getStaticProps() {
     const posts = await getAllPosts({ onlyPost: true })
     const tags = getAllTagsFromPosts(posts)
     return {
       props: {
-        posts,tags,
+        posts,tags
       },
       revalidate: 1
     }
   }
 const test =  props  => {
-  
+
   return<Container >
 {/* 
           <div className=" mx-auto w-56 h-36   overflow-scroll  ">
@@ -45,10 +47,11 @@ const test =  props  => {
           </div>*/}
 
 <div className=' myrotatecard rounded-xl  w-36 h-96 min-w-[10vw] min-h-[10VH]'>
-  123
+123<script> alert(navigator.userAgent) </script>
 </div >
 <div className=' mypingcard rounded-xl  w-36 h-96 min-w-[20vw] min-h-[10VH] relative'>
   <div className='  mymenutext '>333</div >
 </div >
+
 </Container>}
 export default test
