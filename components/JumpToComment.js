df15bf58e026ff0c7eb7d5c7f2c406545db12fd2
Goldeye0351 <goldeye0351@gmail.com>
2023-07-27 18:26:30 +0800

    Create JumpToComment.js

---
+++ components/JumpToComment.js
@@ -0,0 +1,22 @@
+import { ChatIcon} from '@heroicons/react/outline'
+
+/**
+ * 到底部评论区
+ * @returns
+ */
+export default function Jumptocomment() {
+  
+  function navToComment() {
+    if (document.getElementById('comment')) {
+      window.scrollTo({ top: document.getElementById('comment').offsetTop, behavior: 'smooth' })
+    }}
+
+  return (<>
+        <div title={'百分比'}
+            onClick={navToComment} 
+            className="w-6 h-6 cursor-pointer  group-hover:scale-150  rounded-lg flex justify-center items-center duration-200 transition-all" >
+            <ChatIcon className="w-8 h-8"  />
+        </div>
+
+    </>)
+}
