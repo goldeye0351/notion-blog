115d369e0eac17757b29b11b98f8ceb3380282c8
Goldeye0351 <goldeye0351@gmail.com>
2023-07-27 09:43:26 +0800

    Update sb.js

--- pages/sb.js
+++ pages/sb.js
@@ -4,14 +4,16 @@ export default function Iwatch(props) {
 
   
 	return (<Container >
-<div className="flex flex-row overflow-auto ">
+<div className="flex flex-row">
 
-       <div className="  w-3/5 h-96 rounded-3xl p-16 relative">
-			<div className=" ">1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div>
+       <div className="  w-[1000px]  rounded-3xl p-16 bg-slate-300">
+			<div className="  ">1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div>
+			<div className="  ">1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div><div>1</div>
+
 	   </div>
-	   <div className=" w-2/5  rounded-3xl mx-8 space-y-3">
-	   		<div className="  w-56 h-16 rounded-xl bg-red-800  "></div>
-			<div className="  sticky top-0 w-56 h-36 rounded-xl bg-red-100  space-y-3 ">
+	   <div className=" rounded-3xl mx-8 space-y-3">
+	   		<div className="  w-56 h-96 rounded-xl bg-red-800  "></div>
+			<div className="  sticky top-56 w-56  rounded-xl bg-red-100  space-y-3 ">
 				<div className="  w-56 h-8 rounded-xl bg-green-500  "></div>
 				<div className="  w-56 h-8 rounded-xl bg-gray-500  "></div>
 				<TouchMeCard/>
