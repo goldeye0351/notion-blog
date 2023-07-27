fef0edc251945dd5cb5d21fbc6e4d4416acb4155
Goldeye0351 <goldeye0351@gmail.com>
2023-07-27 18:25:36 +0800

    Update Content.js

--- components/Post/Content.js
+++ components/Post/Content.js
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types'
 import FormattedDate from '@/components/Common/FormattedDate'
 import TagItem from '@/components/Common/TagItem'
 import NotionRenderer from '@/components/Post/NotionRenderer'
-import {EyeIcon} from '@heroicons/react/outline'
+import {EyeIcon,ArrowUpIcon,ThumbUpIcon,PencilIcon} from '@heroicons/react/outline'
+import ReadingProgress from '../ReadingProgress'
 import Typed from "typed.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import TableOfContents from './TableOfContents'
+import WechatPay from '@/components/Post/WechatPay'
+import Jumptocomment from '../JumpToComment'
 
 export default function Content (props) {
   const { frontMatter, blockMap, pageTitle,prev,next } = props
-  
+  const [showPay, setShowPay] = useState(false)
+
   useEffect(() => {
 		new Typed('#typed', {
-				strings: [frontMatter.summary],
+				strings: ["ai生成中",frontMatter.summary],
 				typeSpeed: 50,
 				backSpeed: 50,
 				backDelay: 100,  
@@ -22,31 +27,29 @@ export default function Content (props) {
 				loopCount: 3
 			  })})
 
-  return (
-    <article className='flex-none md:overflow-x-visible overflow-x-scroll w-full max-w-5xl '>
-
-      <h1 className='font-bold text-3xl text-black dark:text-white'>
+  return (<div>
+  <div id="biaoti"  className=' flex flex-col justify-center'>
+      <div className='font-bold text-3xl text-black dark:text-white'>
         {pageTitle ? pageTitle : frontMatter.title}
-      </h1>
-
+      </div>
       <div className=' text-black dark:text-white bg-gray-300  dark:bg-gray-600/50  ring-green-300/50 ring-2 p-3 m-8 rounded-xl'>
         AI摘要:<span className='inline-block    ' id='typed' /> 
-
       </div>
-
-        <nav className='flex mt-5 mb-10 items-start text-gray-500 dark:text-gray-400'>
-          <div className='mr-2 mb-4 md:ml-0'>
-            <FormattedDate date={frontMatter.date} />
+      <nav className='flex mt-5 mb-10 items-start text-gray-500 dark:text-gray-400'>
+        <div className='mr-2 mb-4 md:ml-0'>
+          <FormattedDate date={frontMatter.date} />
+        </div>
+        {frontMatter.tags && (
+          <div className='flex flex-nowrap max-w-full overflow-x-auto article-tags'>
+            {frontMatter.tags.map((tag) => (
+              <TagItem key={tag} tag={tag} />
+            ))}
           </div>
-          {frontMatter.tags && (
-            <div className='flex flex-nowrap max-w-full overflow-x-auto article-tags'>
-              {frontMatter.tags.map((tag) => (
-                <TagItem key={tag} tag={tag} />
-              ))}
-            </div>
-          )}
-        </nav>
-
+        )}
+      </nav>
+  </div>
+  <div className=' flex flex-row'>
+    <article  id='postmain'  className='flex-none md:overflow-x-visible overflow-x-scroll w-full max-w-5xl '>
       <div className="-mt-4 relative">
         <NotionRenderer
           blockMap={blockMap}
@@ -54,8 +57,50 @@ export default function Content (props) {
           {...props}
         />
       </div>
-
     </article>
+    <div id="sideright" className='w-56 p-3 hidden xl:block'>  
+          111111
+        <div className=' sticky top-16 '>
+          
+          < TableOfContents frontMatter={frontMatter}  blockMap={blockMap} pageTitle={pageTitle}/>
+          <div className=' flex flex-row justify-between my-3 space-x-3'> 
+
+              <div id="点赞" className='group  w-full p-3  bg-gray-300 dark:bg-gray-600 rounded-2xl flex justify-center mx-auto '>
+                    <button
+                      onClick={() => setShowPay((showPay) => !showPay)}
+                      className=' hover:text-gray-400 dark:hover:text-gray-400'
+                    >
+                      <ThumbUpIcon className='w-6 h-6 ' />
+                    </button>
+              </div>
+              <div id="进度" className='group  bg-gray-300 dark:bg-gray-600 rounded-2xl flex justify-center '>
+                    <ReadingProgress />
+              </div>
+              <div id="我要评论" className='group  w-full p-3  bg-gray-300 dark:bg-gray-600 rounded-2xl flex justify-center mx-auto '>
+                    <Jumptocomment />
+              </div>
+          </div>
+        </div>
+    </div>
+    <div className=' fixed inset-y-[50%] right-0    xl:hidden'>
+      <div className='group   bg-gray-300 dark:bg-gray-600 rounded-2xl flex justify-center '>
+                      <ReadingProgress />
+      </div>
+      <div className='group  w-full p-3  bg-gray-300 dark:bg-gray-600 rounded-2xl flex justify-center mx-auto '>
+        <button
+          onClick={() => setShowPay((showPay) => !showPay)}
+          className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'
+        >
+          <ThumbUpIcon className='w-6 h-6' />
+        </button>
+      </div>
+      <div className='group  w-full p-3  bg-gray-300 dark:bg-gray-600 rounded-2xl flex justify-center mx-auto '>
+                    <Jumptocomment />
+              </div>
+    </div>  
+  </div>
+  {showPay && <WechatPay />}
+</div>
   )
 }
 
