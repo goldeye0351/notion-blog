6391de2baa2609b5005fd2aa785d17c1670b64de
Goldeye0351 <goldeye0351@gmail.com>
2023-07-28 10:39:52 +0800

    Update Content.js

--- components/Post/Content.js
+++ components/Post/Content.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types'
 import FormattedDate from '@/components/Common/FormattedDate'
 import TagItem from '@/components/Common/TagItem'
 import NotionRenderer from '@/components/Post/NotionRenderer'
-import {EyeIcon,ArrowUpIcon,ThumbUpIcon,PencilIcon} from '@heroicons/react/outline'
+import {EyeIcon,ArrowUpIcon,ThumbUpIcon,PencilIcon, DesktopComputerIcon, ChatIcon} from '@heroicons/react/outline'
 import ReadingProgress from '../ReadingProgress'
 import Typed from "typed.js";
 import { useEffect, useState } from "react";
 import TableOfContents from './TableOfContents'
 import WechatPay from '@/components/Post/WechatPay'
 import Jumptocomment from '../JumpToComment'
+import WavesArea from './WavesArea'
 
 export default function Content (props) {
   const { frontMatter, blockMap, pageTitle,prev,next } = props
@@ -17,7 +18,7 @@ export default function Content (props) {
 
   useEffect(() => {
 		new Typed('#typed', {
-				strings: ["ai生成中",frontMatter.summary],
+				strings: ["ai......",frontMatter.summary],
 				typeSpeed: 50,
 				backSpeed: 50,
 				backDelay: 100,  
@@ -29,11 +30,14 @@ export default function Content (props) {
 
   return (<div>
   <div id="biaoti"  className=' flex flex-col justify-center'>
-      <div className='font-bold text-3xl text-black dark:text-white'>
+      <div className='font-bold text-3xl text-black dark:text-white flex justify-center mx-auto'>
         {pageTitle ? pageTitle : frontMatter.title}
       </div>
       <div className=' text-black dark:text-white bg-gray-300  dark:bg-gray-600/50  ring-green-300/50 ring-2 p-3 m-8 rounded-xl'>
-        AI摘要:<span className='inline-block    ' id='typed' /> 
+        <DesktopComputerIcon className='  inline-block h-8'/>
+        <hr/>
+        <ChatIcon className=' inline-block h-6' />
+        <span className='   ' id='typed' /> 
       </div>
       <nav className='flex mt-5 mb-10 items-start text-gray-500 dark:text-gray-400'>
         <div className='mr-2 mb-4 md:ml-0'>
@@ -48,6 +52,7 @@ export default function Content (props) {
         )}
       </nav>
   </div>
+  <WavesArea />
   <div className=' flex flex-row'>
     <article  id='postmain'  className='flex-none md:overflow-x-visible overflow-x-scroll w-full max-w-5xl '>
       <div className="-mt-4 relative">
