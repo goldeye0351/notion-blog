81c8c15e3810179007e19aa21255979e6d22aac3
Goldeye0351 <goldeye0351@gmail.com>
2023-07-28 09:44:21 +0800

    Update blog.js
    调整bolg页面 motion 为左右动作

--- pages/blog.js
+++ pages/blog.js
@@ -12,23 +12,44 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { PlayIcon,ShareIcon } from "@heroicons/react/outline"
 
 const variants = {
-  in: {
-    opacity: 1,
-    scale: 1,
-    y: 0,
+  scaleDown: {
+    scale: 0.8,
+    y: 100,
     transition: {
-      duration: 0.25,
-      delay: 0.25
+      duration: 0.4
     }
   },
   out: {
-    opacity: 0,
-    scale: 0.9,
-    y: 50,
+    x: "-100%",
     transition: {
-      duration: 0.25
+      duration: 0.4,
+      delay: 0.5
     }
-  }
+  },
+  in: {
+    scale: 0.8,
+    y: 100,
+    x: "100%",
+    transition: {
+      duration: 0.4
+    }
+  },
+  center: {
+    x: 0,
+    scale: 0.8,
+    transformOrigin: 'top',
+    transition: {
+      duration: 0.4
+    }
+  },
+  scaleUp: {
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      delay: 0.5
+    }
+  },
 }
 export async function getStaticProps({name}) {
   const posts = await getAllPosts({ onlyPost: true })
@@ -100,65 +121,13 @@ const blog = props => {
         </div>
       </div>
 
-
-{/*
-<div className='flex flex-row space-x-3'>
-    <div className=' md:w-4/5 w-full rounded-lg mb-5 md:mb-8 flex justify-center content-center items-center mx-auto  ' >
-          <swiper-container loop="true" autoplay="true" slides-per-view="1" autoplay-delay="1500"  navigation="true" pagination="true" scrollbar="true" grab-cursor="true" 
-            parallax="true" className="overflow-visible flex  justify-center content-center items-center "
-          > 
-          {posts.map((png) => (<>
-
-            <swiper-slide key={png.id}  className=" flex justify-center content-center items-center ">
-              <Link passHref href={`${BLOG.path}/${png.slug}`} scroll={false}>
-                <div className=" duration-300 overflow-visible h-96 w-96">
-                    <Image src={png?.page_cover}  alt={png.title} fill className=' rounded-3xl'/>
-                </div>
-                <div data-swiper-parallax="-300" data-swiper-parallax-scale="0.05" data-swiper-parallax-duration="600"
-                className=" absolute top-3 mx-auto p-3 text-lg rounded-lg bg-white/80 dark:bg-black/10"   >
-                {png.title}     
-                </div>
-                <div data-swiper-parallax="-600" data-swiper-parallax-scale="0.05" data-swiper-parallax-duration="600"
-                className=" absolute top-16 mx-auto p-3 rounded-lg bg-white/50 dark:bg-black/10"   >
-                {png.summary}     
-                </div>
-              </Link>
-            </swiper-slide>      
-
-            </>
-            ))}
-          </swiper-container>
-    </div>
-    <div className=' card  rounded-xl w-full h-96 relative overflow-hidden'>
-       <div className=' absolute top-3 left-8 rotate-12  '>
-         <marquee width="180" height="380" behavior="scroll" direction="down" >
-
-          <div className=" flex flex-col  space-y-3 ">
-            {posts.map((png) => (<>
-              <Link passHref href={`${BLOG.path}/${png.slug}`} scroll={false}>
-               <Image src={png?.page_cover}  alt={png.title} width={180} height={180} className=' rounded-3xl '/>
-               {png.title}
-              </Link>
-             </>))}
-          </div>
-
-         </marquee>
-
-       </div>
-
-    </div> 
-</div> */}
     <div id="mainmotionblog" >
-      <AnimatePresence
-        initial={false}
-        exitBeforeEnter
-      >
-        <motion.div
-          key={name}
+      <AnimatePresence        initial={false}        exitBeforeEnter      >
+        <motion.div   key={name}
           variants={variants}
-          animate='in'
-          initial='out'
-          exit='out'
+          initial="in"
+          animate={["center", "scaleUp"]}
+          exit={["scaleDown", "out"]}
         >
       {deftag.map((post) => (
         <BlogPost key={post.id} post={post} />
