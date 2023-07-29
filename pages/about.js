1ce33ce310023333b7cfa4b7dd2de6441794d1aa
Goldeye0351 <goldeye0351@gmail.com>
2023-07-29 12:40:02 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -31,16 +31,12 @@ export async function getStaticProps() {
     revalidate: 1
   }
 }
-const About = ({ blockMap }) => {
-    return (
+const About = ({ blockMap,hero }) => {
+  return (
 <Container  title="About Me. Notion Blog" description={BLOG.description} className='about' >
-  <div className="flex flex-col justify-center content-center items-center pb-12  space-y-16">
+  <div className="relative  flex flex-col justify-center content-center items-center pb-12  space-y-16">
     
-     <Tilt 
-              className="my3d shadow-lg shadow-indigo-500 rounded-xl  max-w-[80VW] 
-              bg-[url('../public/mycover.jpg')]
-              bg-cover
-              "
+     <Tilt className="my3d shadow-lg shadow-indigo-500 rounded-xl  max-w-[80VW] "
               perspective={1500}
               glareEnable={true}
               glarePosition={'all'}
@@ -49,10 +45,15 @@ const About = ({ blockMap }) => {
               glareBorderRadius="12px"
               scale={1.02}
             >
-               
+            <Image src={hero.page_cover} alt={hero.title} fill />   
+              <div className=" my3din500  flex mx-auto flex-col text-xl  items-center content-center justify-center p-6 absolute top-[30%] inset-x-[30%]   ">
+              About
+              <Image  src={hero?.page_cover} alt={BLOG.title} width={100} height={30}  className=' rounded-3xl max-h-8  '/>
+
+              </div>
               <div className="my3din flex flex-col justify-center items-center flex-wrap p-12  ">
-                {/*<Image  src={hero?.page_cover} alt={BLOG.title} fill className=' rounded-3xl  '/>*/}
-                <Image  src={Logoimg}  alt='Logo'  width={100}  height={100}  className=' animate-bounce  '/>
+
+
                 <NotionRenderer blockMap={blockMap} frontMatter={{}} subPageTitle={null} />
               </div>
               
