8d44fa003c74db181001830c626b41e2f0e584a3
Goldeye0351 <goldeye0351@gmail.com>
2023-07-30 19:07:47 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -11,29 +11,34 @@ const BlogPost = ({ index , post }) => {
       <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
       <article
           key={post.id}
-          className='group  hover:ring-2  bg-gray-300 dark:bg-gray-600  hover:scale-105 duration-500 overflow-hidden relative my-16  cursor-pointer rounded-xl p-5'
+          className=' bg-gray-300 dark:bg-gray-600  hover:scale-105 duration-500  relative  my-16  cursor-pointer rounded-xl p-5'
         >
-         <div
+         <motion.div
                 id='blog-ID'
                 key={post.id}
-                className={`h-56 w-full md:flex justify-between  ${index % 2 === 1 ? 'flex-row-reverse ' : ''}
-                overflow-hidden rounded-xl `}>
+                className={`group h-56 w-full md:flex justify-between  ${index % 2 === 1 ? 'flex-row-reverse ' : ''}
+                 rounded-xl `}>
 
                 {/* 文字内容 */}
-            <div className='relative my-auto mx-auto md:w-5/12 flex justify-center flex-col items-center  self-center  z-10'>
-                <h2 className='text-lg md:text-xl font-medium mb-2 text-gray-600  dark:text-gray-200  backdrop-blur-sm '>{post.title}</h2>
-                <span className='hidden md:block text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
+            <motion.div
+              initial={false} 
+              whileInView={{ scale: 2 }}
+              className='relative my-auto mx-auto md:w-5/12 flex justify-center flex-col  items-center  self-center z-10'
+              >
+              <div className=' scale-50'>
+                <h2 className='   text-lg md:text-xl font-medium mb-2 text-gray-600  dark:text-gray-200 bg-black/50 md:bg-black/0  backdrop-blur-sm '>{post.title}</h2>
+                <span className='  hidden md:block text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
                   <FormattedDate date={post.date} />
                 </span>
-                <p className='font-light leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
-            </div>
+                <p className='   font-light leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
+              </div>
+            </motion.div>
                 {/* 图片封面 */}
-            <div className="md:relative md:w-7/12 z-0 
-                            w-full h-full overflow-hidden bg-center bg-cover rounded-lg"> 
+            <div className="md:relative md:w-6/12 rounded-lg"> 
                 <Image src={post?.page_cover}  alt={post.title} fill  />
             </div>
 
-            </div>
+        </motion.div>
 
         </article>
       </Link>
