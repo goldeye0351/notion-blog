96b6f08caeeed7a1fffa1bb19f7914b99b989bc5
Goldeye0351 <goldeye0351@gmail.com>
2023-07-30 17:00:09 +0800

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,16 @@
-import { getAllPosts, getAllTagsFromPosts } from '@/lib/notion'
+import { getAllPosts, getAllTagsFromPosts,getAllCatsFromPosts } from '@/lib/notion'
 import SearchLayout from '@/layouts/search'
 
-export default function search({ tags, posts,post }) {
+export default function search({ cats,tags, posts,post }) {
   return <SearchLayout tags={tags} posts={posts}  index={posts.indexOf(post)} />
 }
 export async function getStaticProps() {
   const posts = await getAllPosts({ onlyPost: true })
   const tags = getAllTagsFromPosts(posts)
+  const cats = getAllCatsFromPosts(posts)
   return {
     props: {
-      tags,
+      tags,cats,
       posts
     },
     revalidate: 1
