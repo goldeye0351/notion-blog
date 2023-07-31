46c6c68a762742b37c1e6527b38dbc46c6a7b44b
Goldeye0351 <goldeye0351@gmail.com>
2023-07-31 20:06:46 +0800

    Update Scripts.js

--- components/Common/Scripts.js
+++ components/Common/Scripts.js
@@ -42,6 +42,7 @@ const Scripts = () => (
     )}
     {/*<script async src={'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${BLOG.googleadid}'} crossorigin="anonymous"> </script>*/}
     <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
+    <script async src="https://umami.535251.xyz/script.js" data-website-id="6a873ef2-259b-4e69-a63c-1306d6695bd2"></script>
   </>
 )
 
