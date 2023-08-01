9e45860893016026fb42881e6fc21257918bac83
Goldeye0351 <goldeye0351@gmail.com>
2023-08-01 18:30:24 +0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,39 @@
 import WavesArea from "@/components/Post/WavesArea"
-const my3d = props => {
-  return <div className=' block flex-col justify-center items-center content-center max-w-[100VW] space-y-3'>
-    < WavesArea />
+import BLOG from "@/blog.config";
+import React, { useEffect } from 'react';
 
-</div>}
-export default my3d
+function MyComponent() {
+  useEffect(() => {
+    function umiTongji() {
+      var umiToken = BLOG.analytics.umamiConfig.token
+      var umiId = BLOG.analytics.umamiConfig.websiteId
+      var umiTime = Date.parse(new Date());
+      var umiUrl = "https://umami.mynotion.life/api/websites/"+umiId+"/stats?start_at=1672848000000&end_at="+umiTime;
+      
+      fetch(umiUrl, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+        headers: {
+          'Authorization': 'Bearer ' + umiToken,
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(resdata => {
+        document.querySelector('#pvStatic').innerHTML = resdata.pageviews.value;
+        document.querySelector('#uvStatic').innerHTML = resdata.uniques.value;
+      });
+    }
+
+    umiTongji();
+  }, []);
+
+  return (
+    <div className="tongji">
+      总访问量 <span pvStatic id="pvStatic">0</span> 次 | 总访客数 <span uvStatic id="uvStatic">0</span> 人
+    </div>
+  );
+}
+
+export default  MyComponent
