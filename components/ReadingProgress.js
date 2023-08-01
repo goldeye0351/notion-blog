3e500f1eda79bd3ed29ef3d93e5438b955b61772
Goldeye0351 <goldeye0351@gmail.com>
2023-08-01 09:24:40 +0800

    Update ReadingProgress.js

--- components/ReadingProgress.js
+++ components/ReadingProgress.js
@@ -29,11 +29,11 @@ export default function ReadingProgress() {
   }, [])
 
   return (<>
-        <div title={'百分比'}
+        <div title={'百分比'} data-umami-event="回到顶部" 
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
             className={`${scrollPercentage > 0 ? ' w-6 h-6 m-3 ' : 'w-0 h-0 opacity-0'} 
                          cursor-pointer  group-hover:scale-150  rounded-lg flex justify-center items-center duration-200 transition-all `} >
-            <ArrowSmUpIcon className={'w-8 h-8 hidden group-hover:block'} />
+            <ArrowSmUpIcon  className={'w-8 h-8 hidden group-hover:block'} />
             <div className=' group-hover:hidden  flex justify-center items-center rounded-lg   '>
                 {scrollPercentage < 100 ? scrollPercentage   : <ArrowSmUpIcon className={'w-8 h-8 '} />}
             </div>
