b0f7c062be1d8353714e26205916ec9a035cdc0f
Goldeye0351 <goldeye0351@gmail.com>
2023-08-01 09:24:53 +0800

    Update JumpToComment.js

--- components/JumpToComment.js
+++ components/JumpToComment.js
@@ -15,7 +15,7 @@ export default function Jumptocomment() {
         <div title={'百分比'}
             onClick={navToComment} 
             className="w-6 h-6 cursor-pointer  group-hover:scale-150  rounded-lg flex justify-center items-center duration-200 transition-all" >
-            <ChatIcon className="w-8 h-8"  />
+            <ChatIcon data-umami-event="评论"  className="w-8 h-8"  />
         </div>
 
     </>)
