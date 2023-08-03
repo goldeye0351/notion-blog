74014888a93b5158a7803844e71a620cc36d0c3b
Goldeye0351 <goldeye0351@gmail.com>
2023-08-03 20:06:40 +0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -34,6 +34,10 @@ function MyComponent() {
   return (
     <div className="tongji">
       总访问量 <span pvStatic id="pvStatic">0</span> 次 | 总访客数 <span uvStatic id="uvStatic">0</span> 人
+      <hr/>
+      <span id="busuanzi_container_site_pv" className="hidden">
+          本站总访问量<span id="busuanzi_value_site_pv"></span>次
+      </span>
     </div>
   );
 }
