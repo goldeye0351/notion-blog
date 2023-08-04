551bfe46b2c5f068325892c8e8080030d7027d82
Goldeye0351 <goldeye0351@gmail.com>
2023-08-04 21:15:16 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 import FormattedDate from '@/components/Common/FormattedDate'
 import React from 'react'
+import { EyeIcon } from '@heroicons/react/outline'
 
 const BlogPost = ({ index , post }) => {
   return (
@@ -28,11 +29,12 @@ const BlogPost = ({ index , post }) => {
               <div className=' scale-50'>
                 <h2 className='   text-lg md:text-xl font-medium mb-2 text-gray-600  dark:text-gray-200 bg-black/50 md:bg-black/0  backdrop-blur-sm '>{post.title}</h2>
                 <span className='  hidden md:block text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
-                  <FormattedDate date={post.date} />
+                  <FormattedDate date={post.date} /><EyeIcon className='  ml-8 mr-1 w-6 h-6 inline-block'/>{post.up}
                 </span>
                 <p className='   font-light leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
               </div>
             </motion.div>
+
                 {/* 图片封面 */}
             <div className="md:relative md:w-6/12 rounded-lg"> 
                 <Image src={post?.page_cover}  alt={post.title} fill  />
