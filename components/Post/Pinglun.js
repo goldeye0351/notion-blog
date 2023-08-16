792a90bd824f07d7729e73d8d8ba0d1583ee682d
Goldeye0351 <goldeye0351@gmail.com>
2023-08-16 12:15:49 +0800

    Update Pinglun.js

--- components/Post/Pinglun.js
+++ components/Post/Pinglun.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import FormattedDate from "../Common/FormattedDate";
 import { lang } from '@/public/lang'
+import { RefreshIcon } from "@heroicons/react/outline";
 
 function Pinglun({post,pingluns} ){
     const { locale } = useRouter()
@@ -28,7 +29,9 @@ function Pinglun({post,pingluns} ){
           //alert('已成功.请刷新查看');
           setShowResult(false);
           //ForceRefresh;
-          setTimeout(() => {      router.reload(); }, 8000);
+          setTimeout(() => {      router.reload(); }, 1000);
+          setTimeout(() => {      router.reload(); }, 5000);
+          setTimeout(() => {      router.reload(); }, 10000);
           window.scrollTo({ top: document.getElementById('comment').offsetTop, behavior: 'smooth' })
           //console.log('强制刷新了?')
         } else {
@@ -38,10 +41,13 @@ function Pinglun({post,pingluns} ){
 
 return<>
         <div onClick={ForceRefresh}  className=" flex justify-center cursor-pointer  ">
-          <div className=" max-w-screen-md w-full  ">
+          <div className=" max-w-screen-md tanchukuang  w-full  ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" w-8 ">
               <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
             </svg>
+            <div className="tooltip inline-block absolute  text-center  left-[0%]  bg-gray-300/50  dark:bg-gray-600/50 rounded-xl  p-3 bottom-12 ">
+                     <RefreshIcon  className=" w-6"/>
+            </div>
           </div>
         </div>
   { showResult&& <form onSubmit={submitForm} className=' max-w-screen-md grid grid-cols-4  gap-3  mx-auto'>
