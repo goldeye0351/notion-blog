ded3eee73cb34cd60cade43c0ac48d44526d148f
Goldeye0351 <goldeye0351@gmail.com>
2023-08-16 15:12:00 +0800

    Update Pinglun.js

--- components/Post/Pinglun.js
+++ components/Post/Pinglun.js
@@ -29,8 +29,6 @@ function Pinglun({post,pingluns} ){
           //alert('已成功.请刷新查看');
           setShowResult(false);
           //ForceRefresh;
-          setTimeout(() => {      router.reload(); }, 1000);
-          setTimeout(() => {      router.reload(); }, 5000);
           setTimeout(() => {      router.reload(); }, 10000);
           window.scrollTo({ top: document.getElementById('comment').offsetTop, behavior: 'smooth' })
           //console.log('强制刷新了?')
@@ -46,7 +44,7 @@ return<>
               <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
             </svg>
             <div className="tooltip inline-block absolute  text-center  left-[0%]  bg-gray-300/50  dark:bg-gray-600/50 rounded-xl  p-3 bottom-12 ">
-                     <RefreshIcon  className=" w-6"/>
+               <RefreshIcon  className=" w-6 inline-block"/> 还没看到? Reload
             </div>
           </div>
         </div>
@@ -62,9 +60,9 @@ return<>
           ></textarea>
       </div>
       <div className=" block  col-span-1 ">  </div>
-      <div className=" block  col-span-1 ">  </div>
+      <div className=" hidden sm:block  col-span-1 ">  </div>
 
-      <div className=' col-span-1 p-3  bg-gray-200 dark:bg-gray-700 rounded-xl flex flex-col justify-center ' >
+      <div className=' col-span-2 sm:col-span-1 p-3  bg-gray-200 dark:bg-gray-700 rounded-xl flex flex-col justify-center duration-300 ' >
           <input id="REN"  name="REN"
               type="text" className='  italic px-3  mx-3 block  duration-500 bg-white/30 dark:bg-black/30'
               placeholder={t.LAYOUT.COMMENT_NAME}
@@ -77,7 +75,27 @@ return<>
       {t.LAYOUT.COMMENT_SUBMIT}
       </button>
   </form>}
-
+    {/*  temp    */}
+    <div className="  mx-auto  flex flex-row justify-center">
+      <li  className='  m-3 flex  w-[90vw] max-w-screen-md  '>
+        <div className=" justify-center flex-col flex text-center duration-300 hover:text-blue-500 ">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className=" h-12">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+        </div>
+        <div id="temppl" className=' p-3 bg-gray-200  dark:bg-gray-700 rounded-2xl  text-sm w-full ring-1 ring-white/30    '>
+          {pinglun}
+          <hr />
+          <div className=" flex-row flex justify-between ">
+            <div id="tempname" className=" font-extrabold text-lg ">
+            {ren}
+            </div>
+            <FormattedDate   />
+          </div>
+        </div>
+      </li>
+    </div> 
+   {/*  temp    */}
 <div id="comment" className="  mx-auto  flex flex-row justify-center">
       <ol >
         {pingluns.map((post) => (
