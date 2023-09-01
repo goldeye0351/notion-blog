cd2491ac9a4eae126a44c89e321cd1270c981c43
Goldeye0351 <goldeye0351@gmail.com>
2023-09-01 11:16:27 +0800

    Revert "Update tt.js"
    This reverts commit 5ab025b76e02ed584643e9a671b9c3f7c69e1bbf.

--- pages/tt.js
+++ pages/tt.js
@@ -5,39 +5,23 @@ import React from 'react';
 
 export async function getServerSideProps({ req }) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
   const apiKey = '42f57ba8b461aaa41f1673d23d268d21';
   const response = await fetch(`http://api.ipstack.com/${ip}?access_key=${apiKey}`);
   const data = await response.json();
-  const country = `${data.location.country_flag_emoji}, ${data.region_name}, ${data.city}`;
-  
-  const currentHour = (new Date()).getHours();
-  let greeting;
-
-  if (currentHour >= 0 && currentHour < 6) {
-    greeting = '凌晨好🌙';
-  } else if (currentHour >= 6 && currentHour < 12) {
-    greeting = '早上好🌞';
-  } else if (currentHour >= 12 && currentHour < 18) {
-    greeting = '下午好🌤';
-  } else {
-    greeting = '晚上好🌙';
-  }
+  const country = `${data.country}, ${data.region_name}, ${data.city}`;
 
   return {
     props: {
       ip,
-      country,
-      greeting
+      country
     }
   };
 }
-export default function MyComponent({ ip, country, greeting  }) {
+export default function MyComponent({ ip, country }) {
   return (
     <div>
-
-      <h2>    {greeting}</h2>
-      <h2>欢迎您, 来自 {country} 的客人</h2>
-      <h2>IP: {ip}</h2>
+      <h2>欢迎您, 来自 {country} 的客人，IP地址: {ip}</h2>
     </div>
   );
 }
