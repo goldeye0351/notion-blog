1bca4ec0a1afe003a08da99cc37691ef185c687b
Goldeye0351 <goldeye0351@gmail.com>
2023-09-01 09:40:49 +0800

    Update tt.js

--- pages/tt.js
+++ pages/tt.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
-export default function MyComponent({ ip }) {
-  //const ip2 = window.location.hostname;
-  return (
-    <div>
-      <h2>Your IP address: {ip}</h2>
-      
-    </div>
-  );
-}
+
+
 
 export async function getServerSideProps({ req }) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
+  const apiKey = '42f57ba8b461aaa41f1673d23d268d21';
+  const response = await fetch(`http://api.ipstack.com/${ip}?access_key=${apiKey}`);
+  const data = await response.json();
+  const country = `${data.country}, ${data.region_name}, ${data.city}`;
+
   return {
     props: {
-      ip
+      ip,
+      country
     }
   };
 }
-
+export default function MyComponent({ ip, country }) {
+  return (
+    <div>
+      <h2>欢迎您, 来自 {country} 的客人，IP地址: {ip}</h2>
+    </div>
+  );
+}
